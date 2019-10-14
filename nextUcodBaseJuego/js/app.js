f6cead252c1d960eefe03131dac5837324491414
yluisandres <55513962+yluisandres@users.noreply.github.com>
2019-10-14 14:51:45 +0000

    Add files via upload
    Incrementa eliminación en vertical

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -50,9 +50,12 @@ $(".btn-reinicio").on("click", function()
 {//al hacer click verificamos si esta en Iniciar (0) y determinamos el texto a Reiniciar
   if(contadorReinicio==0)
   {
-    $(".btn-reinicio").text("Reiniciar",inicio())    
+    $(".btn-reinicio").text("Reiniciar",inicio())        
     cargarImagenes()
+    guardarMatriz()  
     verificarIguales()
+    verificarIgualesVertical()
+    evaluarTodo()
     contadorReinicio=1       
   }
   else
@@ -67,10 +70,8 @@ $(".btn-reinicio").on("click", function()
   }  
 })
 var matriz = []
-function cargarImagenes()
-{    
-  ;
-
+function definirMatriz()
+{
   for (var r = 0; r < 6; r++)
   {
      matriz[r]=[];
@@ -79,18 +80,20 @@ function cargarImagenes()
       matriz[r][c]=""
      }
   }
-  
+}
+function cargarImagenes()
+{     
+  definirMatriz()
   for(var i=1;i<8;i++)
   {    
     namecol=".col-"+i    // se le asigna el nombre de la columna mas i
     for(var j=0;j<6;j++)
     {       
-      imagen= Math.floor(Math.random() * (5 - 1)) + 1; //se genera un randómico entre 1 y 4 que son las imagenes existentes
+      var imagen= Math.floor(Math.random() * (5 - 1)) + 1; //se genera un randómico entre 1 y 4 que son las imagenes existentes
       $(namecol).append("<img src='image/"+imagen+".png'/>").sortable();//añade una nueva imagen            
       matriz[j][i-1]="image/"+imagen+".png"      
     }   
-  }  
-  
+  }    
 }
 //cuando cualquier hijo es clickeado se evalua que 3 o mas sean iguales
 var contadorclics=0
@@ -99,8 +102,67 @@ $(".panel-tablero").children().on("click", function()
 {  
   contadorclics++  
   document.getElementById("movimientos-text").innerHTML=contadorclics
+  guardarMatriz()  
   verificarIguales()
+  verificarIgualesVertical()
+  var fila=""
+  for(var i=0;i<6;i++)//i es fila
+  {
+    
+    for(var j=1;j<8;j++)//j es columna
+    {   
+      var columnaact=$(".col-"+j).children();
+      fila=fila+"//"+matriz[i][j-1]   
+      
+      if(matriz[i][j-1]=="-")//buscamos si existen campos vacios en la matriz y si los iguales parten de la fila 0
+      {                     
+        $(columnaact[i]).hide("pulsate",4000,reOrdenar)        
+      }                 
+    }                 
+    fila=fila+"\n"
+  }
+  guardarMatriz()
+  verificarIguales()
+  verificarIgualesVertical()
 })
+
+function evaluarTodo()
+{
+  var fila=""
+  for(var i=0;i<6;i++)//i es fila
+  {
+    
+    for(var j=1;j<8;j++)//j es columna
+    {   
+      var columnaact=$(".col-"+j).children();
+      fila=fila+"//"+matriz[i][j-1]   
+      
+      if(matriz[i][j-1]=="-")//buscamos si existen campos vacios en la matriz y si los iguales parten de la fila 0
+      {                     
+        $(columnaact[i]).hide("pulsate",4000,reOrdenar)        
+      }                 
+    }                 
+    fila=fila+"\n"
+  }
+    
+  verificarIguales()
+  verificarIgualesVertical()
+  
+}
+
+function guardarMatriz()
+{   
+  for(var i=1;i<8;i++)
+  {    
+    var columnaact=$(".col-"+i).children();      
+    for(var j=0;j<6;j++)
+    {      
+      var sourceactual=columnaact[j].src      
+      var srcreal=sourceactual.substring(sourceactual.length-11,sourceactual.length)
+      matriz[j][i-1]=srcreal
+    }   
+  }   
+}
 
 function verificarIguales()
 {
@@ -141,20 +203,67 @@ function verificarIguales()
     }       
   }
 }
-
 function eliminarIguales(cont,varj,vari)
 {
   var desde=(varj-cont)+1
   var hasta=varj    
   for(var i=desde;i<=hasta;i++)
   {              
-    columnaactual=$(".col-"+i).children();                  
+    var columnaactual=$(".col-"+i).children();                  
     matriz[vari][i-1]="-"    
-    $(columnaactual[vari]).hide( "pulsate",2000,reOrdenar)                  
-  }     
-  
+    //$(columnaactual[vari]).hide( "pulsate",2000,reOrdenar)                  
+  }       
   contadorpuntos=contadorpuntos+cont*10
-  document.getElementById("score-text").innerHTML=contadorpuntos   
+  document.getElementById("score-text").innerHTML=contadorpuntos     
+}
+function verificarIgualesVertical()
+{
+  var columnaactual  
+  var contadoriguales=1 
+  var siguiente 
+  for(var i=1; i<8; i++)//i es el número de la columna
+  {     
+    contadoriguales=1
+    columnaactual=$(".col-"+i).children();  
+    for(var j=0;j<6;j++)//j es el número de la fila
+    {       
+      siguiente=j+1 
+      if(siguiente<6)
+      {        
+        if(columnaactual[j].src==columnaactual[siguiente].src)
+        {     
+          contadoriguales++    
+//          alert("Contador= "+contadoriguales+" Fila "+j+" Columna "+i)          
+        }
+        else if(contadoriguales>=3)
+        {
+          eliminarIgualesVertical(contadoriguales,i,j)
+          contadoriguales=1
+        }   
+        else
+        {
+          contadoriguales=1
+        }                     
+      }
+      if(siguiente==6 && contadoriguales>=3)      
+      {
+        eliminarIgualesVertical(contadoriguales,i,j)
+      }
+    }       
+  }
+}
+function eliminarIgualesVertical(cont,vari,varj)
+{//vari es columna varj es fila
+  var desde=(varj-cont)+1
+  var hasta=varj    
+  //var columnaactual=$(".col-"+vari).children();                    
+  for(var k=desde;k<=hasta;k++)
+  {                  
+    matriz[k][vari-1]="-"    
+    //$(columnaactual[i]).hide( "pulsate",2000,reOrdenarVerticales)                  
+  }       
+  contadorpuntos=contadorpuntos+cont*10
+  document.getElementById("score-text").innerHTML=contadorpuntos       
 }
 
 function reOrdenar()
@@ -165,40 +274,25 @@ function reOrdenar()
   } 
   //alert(matriz)
   
-  for(var i=0;i<6;i++)//i es fila
+  for(var i=0;i<7;i++)//i es columna
   {
-    for(var j=0;j<7;j++)//j es columna
+    for(var j=0;j<6;j++)//j es fila
     {
-      if(matriz[i][j]=="-")//buscamos si existen campos vacios en la matriz
-      {
-        if(i>0)//si la fila es mayor a 0 asignamos a la fila afectada el src anterior
-        {
-          //debe ir un for para que recorra hasta la primera fila y remplace la misma
-          for(var k=i;k>=0;k--)
-          {
-            if(k==0)
-            {
-              var imagen= Math.floor(Math.random() * (5 - 1)) + 1; //se genera un randómico entre 1 y 4 que son las imagenes existentes        
-              matriz[k][j]="image/"+imagen+".png" 
-            }
-            else
-            {
-              //alert("Actual "+matriz[k][i]+" Sig "+ matriz[k-1][i])
-              matriz[k][j]=matriz[k-1][j]
-            }            
-          }          
-        }
-        else//si la fila afectada es la primera se generan nuevas imagenes
-        {          
-          var imagen= Math.floor(Math.random() * (5 - 1)) + 1; //se genera un randómico entre 1 y 4 que son las imagenes existentes        
-          matriz[0][j]="image/"+imagen+".png"      
-        }        
+      var imagen= Math.floor(Math.random() * (5 - 1)) + 1; //se genera un randómico entre 1 y 4 que son las imagenes existentes        
+      if(matriz[j][i]=="-" && j==0)//buscamos si existen campos vacios en la matriz y si los iguales parten de la fila 0
+      {                 
+        matriz[j][i]="image/"+imagen+".png"         
       }
-    }
-  }
-  
-  
-  
+      else if(matriz[j][i]=="-")
+      {        
+        for(var k=j;k>0;k--)
+        {
+          matriz[k][i]=matriz[k-1][i]        
+        }            
+        matriz[0][i]="image/"+imagen+".png"      
+      }            
+    }                 
+  }        
   for(var i=1;i<8;i++)
   {    
     namecol=".col-"+i    // se le asigna el nombre de la columna mas i
@@ -208,5 +302,9 @@ function reOrdenar()
       $(namecol).append("<img src='"+src+"'/>").sortable();//añade una nueva imagen                       
     }   
   }
-  verificarIguales()
+  guardarMatriz()
+  //verificarIgualesVertical()
 }
+
+
+
